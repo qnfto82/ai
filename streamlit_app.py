dbb46c6fb82ccd1f7220bc6b7536eb64d09e4253
qnfto82 <qnfto82@gw1.kr>
2025-01-06 15:16:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ content_data = {
             "https://www.youtube.com/watch?v=3JZ_D3ELwOQ"
         ],
         'texts': [
-            "Label 1 관련 첫 번째 텍스트 내용입니다.",
+            "번째 텍스트 내용입니다.",
             "Label 1 관련 두 번째 텍스트 내용입니다.",
             "Label 1 관련 세 번째 텍스트 내용입니다."
         ]
